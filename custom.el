87534edb6e3a6a98e115ad9a9a8298f0d13c71a2
Simon FOREST <simon.forest@loria.fr>
2026-03-02 18:05:14 +0100

    themes

--- custom.el
+++ custom.el
@@ -4,6 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93"
+     "d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d"
+     "9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9"
+     "7de64ff2bb2f94d7679a7e9019e23c3bf1a6a04ba54341c36e7cf2d2e56e2bcc"
+     "166a2faa9dc5b5b3359f7a31a09127ebf7a7926562710367086fcc8fc72145da" default))
  '(helm-candidate-number-limit 500)
  '(helm-locate-command "locate %s -e -A --regex %s")
  '(helm-minibuffer-history-key "M-p")
